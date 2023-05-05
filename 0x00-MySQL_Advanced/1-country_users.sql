35de5284bcb5bb0177b478b768845118bc1026b0
queensk <queenskisivuli@gmail.com>
2023-05-05 03:53:51 +0300

    In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- SQL to Creates a table users
+-- script runs on any database
+CREATE TABLE IF NOT EXISTS `users` (  
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255),
+    `country` ENUM('US', 'CO', 'TN') NOT NULL 
+)
