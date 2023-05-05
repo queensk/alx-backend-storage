9b7100ba1124b97491f040eb87da873591dab4dc
queensk <queenskisivuli@gmail.com>
2023-05-05 04:02:32 +0300

    Best band ever format

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,6 @@
 -- SQL ranks country origins of bands
 -- ordered by the number of non-unique fans
-SELECT DISTINCT `origin`, SUM(`fans`) as `nb_fans` FROM `metal_bands`
+SELECT DISTINCT `origin`, SUM(`fans`) as `nb_fans` 
+FROM `metal_bands`
 GROUP BY `origin`
 ORDER BY `nb_fans` DESC;
