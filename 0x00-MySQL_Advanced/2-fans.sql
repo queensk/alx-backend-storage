fe5cf8b2877958ea56b12692b1cf8cc725ecb6b3
queensk <queenskisivuli@gmail.com>
2023-05-05 03:59:00 +0300

    Best band ever

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
--- SQL script that ranks country origins of bands,
--- ordered by the number of (non-unique) fans
+-- SQL ranks country origins of bands
+-- ordered by the number of non-unique fans
 SELECT DISTINCT `origin`, SUM(`fans`) as `nb_fans` FROM `metal_bands`
 GROUP BY `origin`
 ORDER BY `nb_fans` DESC;
