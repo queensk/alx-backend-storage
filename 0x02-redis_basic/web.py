003a6d928f5f65af7c6c174d9ab2348e287073b5
queensk <queenskisivuli@gmail.com>
2023-05-11 06:15:17 +0300

    used decoretor web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,22 +2,39 @@
 """
 create a web cache
 """
-import redis
 import requests
-rc = redis.Redis()
-count = 0
+import redis
+from functools import wraps
+from typing import Callable, Union
+
+redis_client = redis.Redis()
 
 
-def get_page(url: str) -> str:
+def count_calls(method: Callable) -> Callable:
     """
-    Get a page and cache value
+    count calls
     """
-    rc.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    rc.incr(f"count:{url}")
-    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
-    return resp.text
+    @wraps(method)
+    def wrapper(url: str) -> Union[str, None]:
+        """
+        wraps function
+        """
+        count_key = "count:{}".format(url)
+        result_key = "result:{}".format(url)
 
+        cached_result = redis_client.get(result_key)
+        if cached_result:
+            redis_client.incr(count_key)
+            return cached_result.decode("utf-8")
 
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+        response = requests.get(url)
+        html_content = response.text
+
+        redis_client.setex(result_key, 10, html_content)
+        redis_client.set(count_key, 1)
+
+        return html_content
+
+@count_calls
+def get_page(url):
+    return requests.get(url).text
