7a15791c45de452360830eecb0be43325bc8ab4b
queensk <queenskisivuli@gmail.com>
2023-05-11 05:21:07 +0300

    method key update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,12 +12,13 @@ def count_calls(method: Callable) -> Callable:
     """
     counts method call
     """
+    key = method.__qualname__
+
     @wraps(method)
     def wrapper(self, *args, **kwargs) -> Union[str, bytes, int, float]:
         """
         wrapper function
         """
-        key = method.__qualname__
         self._redis.incr(key)
         return method(self, *args, **kwargs)
     return wrapper
