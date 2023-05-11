9efabcbcfe265fe0ff7f28ffee1e41c1ec172426
queensk <queenskisivuli@gmail.com>
2023-05-11 05:09:32 +0300

    test Incrementing values

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -8,21 +8,28 @@ Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-data = b"hello"
-key = cache.store(data)
-print(key)
+# data = b"hello"
+# key = cache.store(data)
+# print(key)
 
-local_redis = redis.Redis()
-print(local_redis.get(key))
+# local_redis = redis.Redis()
+# print(local_redis.get(key))
 
-cache = Cache()
+# cache = Cache()
 
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
+# TEST_CASES = {
+#     b"foo": None,
+#     123: int,
+#     "bar": lambda d: d.decode("utf-8")
+# }
 
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
+# for value, fn in TEST_CASES.items():
+#     key = cache.store(value)
+#     assert cache.get(key, fn=fn) == value
+
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
+
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
