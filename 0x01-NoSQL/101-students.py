8701597a26f6f89c0619ce360e63b5c8f243e34b
queensk <queenskisivuli@gmail.com>
2023-05-13 14:46:20 +0300

    update sort pupeline

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -7,8 +7,18 @@ def top_students(mongo_collection):
     Returns all students sorted by average score
     """
     pipeline = [
-        {"$unwind": "$scores"},
-        {"$group": {"_id": "$_id", "name": {"$first": "$name"}, "averageScore": {"$avg": "$scores.score"}}},
-        {"$sort": {"averageScore": -1}}
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
     ]
     return mongo_collection.aggregate(pipeline)
