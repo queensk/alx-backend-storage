5e6892d25efa976f07d1dee285e76260698fbd7a
queensk <queenskisivuli@gmail.com>
2023-05-13 12:59:39 +0300

    all documents in python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+List all documents in Python
+"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection
+    """
+    if not mongo_collection:
+        return []
+    return [mongo_collection.find()]
