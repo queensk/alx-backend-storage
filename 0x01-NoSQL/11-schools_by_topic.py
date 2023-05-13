09cfcd692d414f33a0f72aeff2b5ca3d1e1df6bc
queensk <queenskisivuli@gmail.com>
2023-05-13 13:44:12 +0300

    Where can I learn Python?

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Where can I learn Python?
+"""
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of school having a specific topic
+    """
+    schools = []
+    query = mongo_collection.find({"topics": {"$in": [topic]}})
+    for school in query:
+        schools.append(school)
+    return schools
